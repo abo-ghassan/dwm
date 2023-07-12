a8c240738ac7d8bc9ae9ef824172822c6861759e
Sa'eed AL_Habal <asaeedalhabal@gmail.com>
2023-07-12 14:07:53 +0300

    changed the hex codes of the colors to 3 digits

--- config.h
+++ config.h
@@ -11,25 +11,24 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 10;       /* gaps between windows */
+static const unsigned int gappx     = 5;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "Monospace:size=11", "NotoColorEmoji:pixelsize=20" };
-/* static const char dmenufont[]       = "Monospace:size=11"; */
+static const char *fonts[]          = { "Monospace:size=11" };
 
-static const char ui_color[4][8]   = {
-	"#660066", //main
-	"#222222", //unfocus main
-	"#bfbfbf", //text
-	"#bfbfbf"  //unfocus text
+static const char ui_color[4][8]    = {
+	"#606", //main
+	"#222", //unfocus main
+	"#ddd", //text
+	"#ddd"  //unfocus text
 };
 static const char *colors[][3]      = {
-	/*               fg           bg           border   */
+	/*               fg           bg           border      */
 	[SchemeNorm] = { ui_color[3], ui_color[1], ui_color[1] },
-	[SchemeSel]  = { ui_color[2], ui_color[0], ui_color[0] },
+	[SchemeSel]  = { ui_color[2], ui_color[0], ui_color[0] }
 };
 
 /* tagging */
@@ -77,7 +76,6 @@ static const char *termcmd[]  = { "st", NULL };
 /* static const char *browser[] = {"firefox", NULL}; */
 /* static const char *emoji[] = {"emoji",NULL}; */
 /* static const char *keyboard_layout[] = {"change_layout",NULL}; */
-/* static const char *lockscreen[] = {"betterlockscreen", "-l", "dimblur",NULL}; */
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -86,7 +84,6 @@ static const Key keys[] = {
 	/* { MODKEY,                       XK_b,      spawn,  	   {.v = browser } }, */
 	/* { MODKEY,                       XK_e,	   spawn,  	   {.v = emoji } }, */
 	/* { ShiftMask,                    XK_Alt_L,  spawn,  	   {.v = keyboard_layout } }, */
-	/* { MODKEY|ShiftMask,             XK_Tab,    spawn,  	   {.v = lockscreen } }, */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
